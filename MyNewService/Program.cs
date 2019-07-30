50aedd91569e6ad121319b8f4b2e98be3b8c44e4
Toros Gökkurt <torosgo@microsoft.com>
2019-07-30 14:11:02 +0300

    added MyNewService with eventlog and installer

--- MyNewService/Program.cs
+++ MyNewService/Program.cs
@@ -17,7 +17,7 @@ namespace MyNewService
 			ServiceBase[] ServicesToRun;
 			ServicesToRun = new ServiceBase[]
 			{
-				new Service1()
+				new MyNewService()
 			};
 			ServiceBase.Run(ServicesToRun);
 		}
